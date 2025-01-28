0af1552c60fa0c5735dd2988d99aca5043d30c42
siroj05 <rojudin123@gmail.com>
2025-01-28 18:08:00 +0700

    fix userInfo response

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -1,9 +1,10 @@
 // posts-controller.js
 import { createPosts } from "../model/posts-model.js";
 import { getDb } from "../db/db.js";
+import { ObjectId } from "mongodb";
 export const handleCreatePost = async (req, res) => {
-  const { cover, title, content, userId } = req.body;
-
+  let { cover, title, content, userId } = req.body;
+  userId = new ObjectId(userId)
   try {
     // Validasi input
     if (!cover || !title || !content || !userId) {
@@ -28,13 +29,51 @@ export const handleCreatePost = async (req, res) => {
 };
 
 export const getPosts = async (req, res) => {
-  const db = getDb()
+  const db = getDb();
   try {
     const collection = db.collection('posts');
-    const result = await collection.find({}).toArray();
-    res.json(result);
+
+    // Tahap untuk mengonversi userId menjadi ObjectId
+    const lookupUserInfo = {
+      $lookup: {
+        from: 'users', // Nama koleksi yang akan di-join
+        localField: 'userId', // Field di posts
+        foreignField: '_id', // Field di users
+        as: 'userInfo', // Nama field hasil join
+      },
+    };
+
+    // Tahap untuk membuka array userInfo menjadi objek
+    const unwindUserInfo = {
+      $unwind: {
+        path: '$userInfo', // Membuka array userInfo
+        preserveNullAndEmptyArrays: true, // Tetap sertakan data meskipun user tidak ditemukan
+      },
+    };
+
+    const rawData = {
+      $project:{
+        _id:1,
+        title:1,
+        userId:1,
+        cover:1,
+        content:1,
+        'userInfo.firstName':1,
+        'userInfo.email':1,
+        'userInfo._id':1
+      }
+    }
+
+    const pipeline = [
+      lookupUserInfo,
+      unwindUserInfo,
+      rawData
+    ]
+
+    const result = await collection.aggregate(pipeline).toArray()
+    res.json(result); // Kirim hasil ke frontend
   } catch (error) {
-    console.error("Error saat mengambil data:", error);
-    res.status(500).send("Terjadi kesalahan server");
+    console.error('Error saat mengambil data:', error);
+    res.status(500).send('Terjadi kesalahan server');
   }
-}
+};
